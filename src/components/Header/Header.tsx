88f6b9b8e5bfffde7db82a47a24e832d017df24b
Anna Cheshok <anna_cheshok@mail.ru>
2024-04-14 14:45:01 +0300

    add footer

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,9 +5,10 @@ import icon from '../../shared/assets/icon.png';
 import arrow from '../../shared/assets/arrow.png';
 import { Menu } from './_ui';
 import { Button } from '../Button';
-import { IHeader } from './Header.types';
+import { IHeaderProps } from './Header.types';
+import { Link } from 'react-router-dom';
 
-const Header = ({src} :IHeader) => {
+const Header = ({ src }: IHeaderProps) => {
     const styles = {
         headerContainer: cn('flex items-center h-[90px] justify-between'),
         logo: cn('w-[129px] h-[40px] mt-2'),
@@ -16,29 +17,30 @@ const Header = ({src} :IHeader) => {
         name: cn('font-medium text-[#eceef2] ml-[8px]'),
         arrow: cn('absolute top-[30px] right-[16px] cursor-pointer'),
         drop: cn('hidden group-hover:block absolute top-[75px] text-[#fff] left-0 text-xl font-normal rounded-b-2xl w-[321px] bg-[#216f75] p-[18.5px] pb-[2.5px]'),
-        dropli: cn ('mb-[16px] cursor-pointer'),
+        dropli: cn('mb-[16px] cursor-pointer'),
     };
+
     return (
-        <div className={`container ${styles.headerContainer}`}>
+        <header className={`container ${styles.headerContainer}`}>
             <img src={src == 'light' ? logoLight : logoDark} alt='' className={styles.logo} />
             {src == 'light' ? <Menu /> : ''}
-            {src == 'light' ? 
-            <div className={styles.buttons}>
-                <Button title='Регистрация' type='button' className='mr-6' />
-                <Button title='Вход' type='button' outlined />
-            </div> : 
-            <div className={`group ${styles.menu}`}>
-                <img src={icon} alt="" />
-                <span className={styles.name}>Иван</span>
-                <img className={styles.arrow} src={arrow} alt="" />
-                <ul className={styles.drop}>
-                  <li className={styles.dropli}>Сервис распознования</li>
-                  <li className={styles.dropli}>История распознования</li>
-                  <li className={styles.dropli}>Настройки профиля</li>
-                  <li className={styles.dropli}>Выход</li>
-                </ul>
-            </div>}
-        </div>
+            {src == 'light' ?
+                <div className={styles.buttons}>
+                    <Button title='Регистрация' type='button' className='mr-6' />
+                    <Button title='Вход' type='button' outlined />
+                </div> :
+                <div className={`group ${styles.menu}`}>
+                    <img src={icon} alt="" />
+                    <span className={styles.name}>Иван</span>
+                    <img className={styles.arrow} src={arrow} alt="" />
+                    <ul className={styles.drop}>
+                        <li className={styles.dropli}><Link to="recognition_service">Сервис распознования</Link></li>
+                        <li className={styles.dropli}><Link to="history_recognition">История распознования</Link></li>
+                        <li className={styles.dropli}><Link to="settings">Настройки профиля</Link></li>
+                        <li className={styles.dropli}>Выход</li>
+                    </ul>
+                </div>}
+        </header>
 
     );
 };
