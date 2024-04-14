efffb477357d3fe3685b28d4eebe9f831d5bb3ce
Anastasia <nastya.pytliak@gmail.com>
2024-04-14 04:53:57 +0300

    recognition service

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,43 @@
 import cn from 'clsx'
 import logoDark from '../../shared/assets/logo-dark.png';
 import logoLight from '../../shared/assets/logo-light.png';
+import icon from '../../shared/assets/icon.png';
+import arrow from '../../shared/assets/arrow.png';
 import { Menu } from './_ui';
 import { Button } from '../Button';
+import { IHeader } from './Header.types';
 
-const Header = () => {
+const Header = ({src} :IHeader) => {
     const styles = {
         headerContainer: cn('flex items-center h-[90px] justify-between'),
         logo: cn('w-[129px] h-[40px] mt-2'),
-        buttons: cn('flex')
+        buttons: cn('flex'),
+        menu: cn('flex relative items-center bg-[#216f75] text-2xl rounded-b-2xl w-[321px] p-[16px] pt-[22px] mt-[-5px]'),
+        name: cn('font-medium text-[#eceef2] ml-[8px]'),
+        arrow: cn('absolute top-[30px] right-[16px] cursor-pointer'),
+        drop: cn('hidden group-hover:block absolute top-[75px] text-[#fff] left-0 text-xl font-normal rounded-b-2xl w-[321px] bg-[#216f75] p-[18.5px] pb-[2.5px]'),
+        dropli: cn ('mb-[16px] cursor-pointer'),
     };
     return (
         <div className={`container ${styles.headerContainer}`}>
-            <img src={logoLight} alt='' className={styles.logo} />
-            <Menu />
+            <img src={src == 'light' ? logoLight : logoDark} alt='' className={styles.logo} />
+            {src == 'light' ? <Menu /> : ''}
+            {src == 'light' ? 
             <div className={styles.buttons}>
                 <Button title='Регистрация' type='button' className='mr-6' />
                 <Button title='Вход' type='button' outlined />
-            </div>
+            </div> : 
+            <div className={`group ${styles.menu}`}>
+                <img src={icon} alt="" />
+                <span className={styles.name}>Иван</span>
+                <img className={styles.arrow} src={arrow} alt="" />
+                <ul className={styles.drop}>
+                  <li className={styles.dropli}>Сервис распознования</li>
+                  <li className={styles.dropli}>История распознования</li>
+                  <li className={styles.dropli}>Настройки профиля</li>
+                  <li className={styles.dropli}>Выход</li>
+                </ul>
+            </div>}
         </div>
 
     );
--- src/pages/RecognitionService/RecognitionService.tsx
+++ src/pages/RecognitionService/RecognitionService.tsx
@@ -1,5 +1,31 @@
+import { Button, Header } from "../../components";
+import play from '../../shared/assets/play.png';
+import birdSecond from '../../shared/assets/bird-2.png';
+
 const RecognitionService = () => {
-    return <>recognition service</>
+    return (
+      <>
+        <Header src="dark" />
+         <section className="container pt-[64px] pb-[64px] bg-no-repeat bg-right-bottom" style={{ backgroundImage: `url(${birdSecond})` }}>
+            <div className="title font-medium text-[32px] mb-[32px] text-[#236b71]">Сервис распознования</div>
+            <form className="uploads w-full max-w-[613px] bg-[#ded8bc] rounded-3xl p-[22px] pb-[18px]">
+              <label className="block bg-[#216f75] opacity-40 h-[240px] rounded-[18px] mb-[-27px] text-xl font-medium text-[#eef0f5]">
+                <span className="flex justify-center items-center h-full cursor-pointer">Перетащите файл сюда</span>
+                <input className="hidden" type="file" id="img" accept="audio/*" />
+              </label>
+              <span className="block w-full h-[27px] bg-[#619296] rounded-[16px]"></span>
+              <div className="flex items-center mt-[4px] text-[#1b1810]">
+                <img className="mr-[6px]" src={play} alt="" />
+                <span>00:00:00</span>
+              </div>
+              <div className="pt-[18px] text-center">
+                <Button title='Загрузить файл' type='submit' className='mr-6' />
+                <Button title='Начать распознование' type='button' outlined className='text-[#236b71] hover:text-[#fff]'/>
+              </div>
+            </form>
+          </section>
+      </> 
+  )
 }
 
 export default RecognitionService;
